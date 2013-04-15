921921010aca637ab13ca364528ce5753f116711
Robert Smith <r2theob23@gmail.com>
2013-04-14 23:22:34 -0400

    Used the blue angles example from the documentation in my twitter search query, works except getting a 403 status on all profile image urls….good stopping point for tonight. Long week ahead

--- Week2/ios/www/js/main.js
+++ Week2/ios/www/js/main.js
@@ -2,3 +2,21 @@
 //AVF Term 1304
 //Week 2
 //main.js
+
+$(function(){
+	
+	$.getJSON("http://search.twitter.com/search.json?q=blue%20angels&rpp=5", 
+		function(data){
+			console.log(data)
+			$("#data-msg").html("<p>Data Obtained</p>")
+			for(i=0, j=data.results.length; i<j; i++){
+				$("#data-output")
+					.append("<li>" + 
+						"<p>" + "<img src=" + data.results[i].profile_image_url + "/><br />" + 
+							data.results[i].text + ", <em>" + data.results[i].created_at + "</em>" + 
+								"</p>" + 
+									"</li>");			
+				}
+		});
+});
+
