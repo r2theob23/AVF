26cb025e7aa96e766a1fcdc29139fc23b3172b89
Robert Smith <r2theob23@gmail.com>
2013-04-17 21:39:27 -0400

    finished twitter api, moving on to google +

--- Week2/ios/www/js/main.js
+++ Week2/ios/www/js/main.js
@@ -5,15 +5,15 @@
 
 $(function(){
 	
-	$.getJSON("http://search.twitter.com/search.json?q=blue%20angels&rpp=5", 
+	$.getJSON("http://search.twitter.com/search.json?q=boston%20bombings&rrp=50&result_type=resent", 
 		function(data){
 			console.log(data)
-			$("#data-msg").html("<p>Data Obtained</p>")
+			$("#heading").html("<p>Recent Tweets Regarding Tragedy in Boston</p>")
 			for(i=0, j=data.results.length; i<j; i++){
-				$("#data-output")
+				$("#tweets")
 					.append("<li>" + 
-						"<p>" + "<img src=" + data.results[i].profile_image_url + "/><br />" + 
-							data.results[i].text + ", <em>" + data.results[i].created_at + "</em>" + 
+						"<p>" + "<img src='" + data.results[i].profile_image_url + "' /><br />" + 
+							data.results[i].text + ", <em>" + data.results[i].created_at +"</em>" + 
 								"</p>" + 
 									"</li>");			
 				}
