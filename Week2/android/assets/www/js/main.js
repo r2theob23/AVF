be80c8c766670bf21b32184a3725cf9203a8c271
Robert Smith <r2theob23@gmail.com>
2013-04-18 23:35:58 -0400

    Added files to android folder

---
+++ Week2/android/assets/www/js/main.js
@@ -0,0 +1,34 @@
+//Robert Smith
+//AVF Term 1304
+//Week 2
+//main.js
+
+$(function(){
+	
+	$.getJSON("http://search.twitter.com/search.json?q=boston%20bombings&rpp=5&include_entities=true&result_type=recent&callback=?", 
+		function(data){
+			console.log(data)
+			
+$("#heading").html("<p>Recent Tweets Regarding Tragedy in Boston</p>")
+			for(i=0, j=data.results.length; i<j; i++){
+				$("#tweets")
+					.append("<li>" + 
+						"<p>" + "<img src='" + data.results[i].profile_image_url + "' /><br />" + 
+							data.results[i].text + ", <em>" + data.results[i].created_at +"</em>" + 
+								"</p>" + 
+									"</li>");			
+				}
+		});
+});
+
+
+$("#googlePage").on("pageinit", function(){
+	var mapOptions = {
+    center: new google.maps.LatLng(37.7831,-122.4039),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+new google.maps.Map(document.getElementById('map'), mapOptions);
+
+});
